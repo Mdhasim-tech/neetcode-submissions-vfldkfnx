e4e13cc3cf6b8564cfb8081731376386828fad14
Mdhasim-tech <131744384+Mdhasim-tech@users.noreply.github.com>
2026-04-13 18:49:27 +0530

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int n;
+public:
+    bool func(int ind,int target,vector<int>&nums,vector<vector<int>>&dp){
+        if(ind<0)return target==0;
+        if(dp[ind][target]!=-1)return dp[ind][target];
+        bool notpick=func(ind-1,target,nums,dp);
+        bool pick=false;
+        if(nums[ind]<=target){
+            pick=func(ind-1,target-nums[ind],nums,dp);
+        }
+        return dp[ind][target]=notpick||pick;
+    }
+    bool canPartition(vector<int>& nums) {
+        n=nums.size();
+        int total=0;
+        for(auto it:nums)total+=it;
+        if(total%2!=0)return false;
+        int target=total/2;
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return func(n-1,target,nums,dp);
+    }
+};
